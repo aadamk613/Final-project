eb4b31fad1db81d9e305794fddc353d7f17a3e7d
parsifal295 <parsifal295@gmail.com>
2023-11-13 14:38:32 +0900

    Update MemberServiceImpl.java

--- Final-project/src/main/java/com/kh/finalproject/member/model/service/MemberServiceImpl.java
+++ Final-project/src/main/java/com/kh/finalproject/member/model/service/MemberServiceImpl.java
@@ -1,7 +1,10 @@
 package com.kh.finalproject.member.model.service;
 
+import org.springframework.stereotype.Service;
+
 import com.kh.finalproject.member.model.vo.Member;
 
+@Service
 public class MemberServiceImpl implements MemberService {
 
   @Override
